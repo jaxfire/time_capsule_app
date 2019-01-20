e65f47ad842012ca1cd0214bc0a66b340edec58c
JAsh85 <jamesash85@gmail.com>
2019-01-20 14:04:58 +0000

    Add Bloc Provider, DayEntryBloc and plug in to the UI.

---
+++ lib/blocs/daily_memories_bloc.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+
+import 'package:time_capsule_app/blocs/bloc_provider.dart';
+
+abstract class DateChangeEvent {
+  int changeValue();
+}
+
+class DateChangeIncrement extends DateChangeEvent {
+  @override
+  int changeValue() {
+    return 1;
+  }
+}
+
+class DateChangeDecrement extends DateChangeEvent {
+  @override
+  int changeValue() {
+    return -1;
+  }
+}
+
+class DayEntry {
+  String entry1, entry2, entry3;
+  DayEntry(this.entry1, this.entry2, this.entry3);
+}
+
+class DayEntryBloc implements BlocBase {
+
+  ///
+  /// Test data
+  ///
+  List<DayEntry> _dayEntries = List();
+  int date = 0;
+
+  // ##########  STREAMS  ##############
+  ///
+  /// UPSERT day memory entries
+  ///
+  final _dayEntryAddController = StreamController<String>();
+  Sink<String> get inAddDayEntry => _dayEntryAddController.sink; // Input from button handlers
+
+
+  ///
+  /// GET day memory entries
+  ///
+  final _dayEntryController = StreamController<String>();
+  Sink<String> get _inDayEntry =>_dayEntryController.sink; // Input from Cloud Firestore
+  Stream<String> get outDayEntry =>_dayEntryController.stream; // Output to whoever subscribes
+
+  ///
+  /// CHANGE DATE
+  ///
+  final _dateChangeController = StreamController<DateChangeEvent>();
+  Sink<DateChangeEvent> get inDateChange => _dateChangeController.sink;
+
+
+  ///
+  /// Constructor
+  ///
+  DayEntryBloc(){
+
+    // Test data
+    _dayEntries.add(DayEntry('a','b','c'));
+    _dayEntries.add(DayEntry('d','e','f'));
+
+
+    _dayEntryAddController.stream.listen(_handleAddDayEntry);
+    _dateChangeController.stream.listen(_handleDateChange);
+  }
+
+  void dispose(){
+    _dayEntryAddController.close();
+    _dayEntryController.close();
+    _dateChangeController.close();
+  }
+
+  // ############# HANDLING  #####################
+
+  void _handleAddDayEntry(String inputData){
+    print('Handling input of $inputData');
+    // Persist the day entry
+//    _favorites.add(movieCard);
+
+    _notify();
+  }
+
+  void _handleDateChange(DateChangeEvent dateChangeEvent){
+    print('Handling date change');
+    int requestedDate = date + dateChangeEvent.changeValue();
+    if(requestedDate >= 0 && requestedDate <= 2) {
+      print('Date is in range for test purposes');
+      date = requestedDate;
+      // TODO:
+      // Check if we already have this date entry in memory.
+      // Make request to firebase for new date.
+    }
+  }
+
+  void _notify(){
+    // Send to whomever is interested...
+  }
+}
